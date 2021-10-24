30df1383be9b8c14924357dfd1cf9f4e4de858d4
Luiz Almeida <almeidakk@hotmail.com>
2021-10-24 18:50:08 -0300

    Versão de entrega

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,29 @@
 // Seu código vai aqui
+let myArray = []
+
+function snapCrackle(maxValue) {
+
+    for (counter = 1; counter <= maxValue; counter++) {
+        if (counter % 2 !== 0 && counter % 5 === 0) {
+
+            myArray.push("SnapCrackle, ")
+
+        } else if (counter % 2 !== 0) {
+
+            myArray.push("Snap, ")
+
+        } else if (counter % 5 === 0) {
+
+            myArray.push("Crackle, ")
+
+        } else if (counter % 2 === 0 && counter % 5 !== 0) {
+
+            myArray.push(counter)
+
+        }
+    }
+    return myArray
+
+}
+
+snapCrackle(12)
