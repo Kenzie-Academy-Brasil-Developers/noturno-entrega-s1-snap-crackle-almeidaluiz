1b890019d2e8429ebdd81722d03d591ee7e36263
Luiz Almeida <almeidakk@hotmail.com>
2021-10-30 14:36:40 -0300

    Ajuste para retornar string

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -6,15 +6,15 @@ function snapCrackle(maxValue) {
     for (counter = 1; counter <= maxValue; counter++) {
         if (counter % 2 !== 0 && counter % 5 === 0) {
 
-            myArray.push("SnapCrackle, ")
+            myArray.push("SnapCrackle")
 
         } else if (counter % 2 !== 0) {
 
-            myArray.push("Snap, ")
+            myArray.push("Snap")
 
         } else if (counter % 5 === 0) {
 
-            myArray.push("Crackle, ")
+            myArray.push("Crackle")
 
         } else if (counter % 2 === 0 && counter % 5 !== 0) {
 
@@ -26,4 +26,4 @@ function snapCrackle(maxValue) {
 }
 
 snapCrackle(15)
-console.log(myArray)
+console.log(myArray.join(', '));
